a2c8731149ef80874b2fb87944ef8de3d1ad9be9
ckahub <ahuja13ck@gmail.com>
2021-09-20 03:53:56 +0530

    feast
    given logic to input taken from user

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const billAmount= document.querySelector("#bill_amount");
 const cashGiven= document.querySelector("#cash_given");
 const check = document.querySelector("#check");
 const message = document.querySelector("#message");
+const NumberOfNotes= document.querySelector(".No_of_notes");
+
+const avaialableNotes=[2000,500,100,50,10,5];
 
 check.addEventListener("click", function validate(){
 
@@ -38,7 +41,17 @@ function hideMessage()
 {
     message.style.display = "none";
 }
-function calculateChange(change)
+function calculateChange(amountToReturn)
 {
+    for(let i=0;i<availableNotes.length;i++)
+    {
+        const Notes= Math.trunc(amountToReturn/availableNotes[i]);
+       
+        amountToReturn= amountToReturn % availableNotes[i];
+        NumberOfNotes[i].innerHTML=Notes;
 
+       
+
+
+    }
 }
