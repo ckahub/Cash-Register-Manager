a2b7f119ae0eeae41504e6dbe4b05827e44d57df
ckahub <ahuja13ck@gmail.com>
2021-09-29 12:54:14 +0530

    refactor

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const billAmount= document.querySelector("#bill_amount");
 const cashGiven= document.querySelector("#cash_given");
 const chck = document.querySelector("#check");
 const message = document.querySelector("#message");
-const NumberOfNotes= document.querySelector(".Number_Of_Notes");
+const NumberOfNotes= document.querySelectorAll(".Number_Of_Notes");
 
 const availableNotes= [ 2000 , 500 , 100 , 50 , 10 , 5 , 1 ];
+var amountToReturn=Number(billAmount.value-cashGiven.value);
 
 chck.addEventListener("click", function validate(){
     hideMessage();
@@ -13,7 +14,7 @@ if(billAmount.value>0)
     hideMessage();
     if(billAmount.value<cashGiven.value)
 {
-    var amountToReturn=billAmount-cashGiven;
+    
     calculateChange(amountToReturn);
 }
     else
@@ -42,17 +43,20 @@ function hideMessage()
 {
     message.style.display = "none";
 }
-function calculateChange(amountToReturn)
+function calculateChange( amountToReturn)
 {
     for(let i=0;i<availableNotes.length;i++)
     {
-        const Notes= Math.trunc( amountToReturn/ availableNotes[i]);
+        var Notes= Math.trunc( amountToReturn / availableNotes[i]);
        
-        amountToReturn= amountToReturn % availableNotes[i];
-        NumberOfNotes[i] =Notes;
+        amountToReturn = amountToReturn % availableNotes[i];
+        console.log(Notes);
+        NumberOfNotes[i].innerText = Notes;
+      
 
        
 
 
     }
+
 }
