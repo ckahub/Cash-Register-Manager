a0350e95b5ae0c64bc3290a297b7b51498bfe64c
ckahub <ahuja13ck@gmail.com>
2021-10-04 03:43:44 +0530

    refactor

--- app.js
+++ app.js
@@ -2,37 +2,34 @@ const billAmount= document.querySelector("#bill_amount");
 const cashGiven= document.querySelector("#cash_given");
 const chck = document.querySelector("#check");
 const message = document.querySelector("#message");
-const NumberOfNotes= document.querySelectorAll(".Number_Of_Notes");
+const noOfNotes= document.querySelectorAll(".Number_Of_Notes");
 
 const availableNotes= [ 2000 , 500 , 100 , 50 , 10 , 5 , 1 ];
-var amountToReturn=Number(billAmount.value-cashGiven.value);
 
-chck.addEventListener("click", function validate(){
+
+chck.addEventListener("click", validate);
+
+function validate() {
     hideMessage();
-if(billAmount.value>0)
-{
-    hideMessage();
-    if(billAmount.value<cashGiven.value)
-{
-    
-    calculateChange(amountToReturn);
-}
-    else
-    {
-        message.style.display = "block";
-        showMessage("Do you want to wash plates??");
+    if (billAmount.value > 0)
+     {
+
+      if (cashGiven.value >= billAmount.value) {
+       
+        const amountToReturn = cashGiven.value - billAmount.value; 
+        calculateChange(amountToReturn);
+      } 
+      else 
+      {
+        showMessage("Do you wanna wash plates?");
+      }
     }
-
-}    
-
-
-else
-{
-    message.style.display = "block";
-    showMessage("Not a valid amount");
-}
-})
-
+     else {
+      showMessage("Invalid Bill Amount");
+    }
+  };
+  
+  
 function showMessage(msg)
 {
     message.style.display = "block";
@@ -43,20 +40,20 @@ function hideMessage()
 {
     message.style.display = "none";
 }
-function calculateChange( amountToReturn)
-{
-    for(let i=0;i<availableNotes.length;i++)
-    {
-        var Notes= Math.trunc( amountToReturn / availableNotes[i]);
-       
-        amountToReturn = amountToReturn % availableNotes[i];
-        console.log(Notes);
-        NumberOfNotes[i].innerText = Notes;
+
+
+function calculateChange(amountToReturn) {
+    
+    for (let i = 0; i < availableNotes.length; i++) {
       
-
-       
-
-
+      const numberOfNotes = Math.trunc(amountToReturn / availableNotes[i]);
+     
+  
+      
+      amountToReturn = amountToReturn % availableNotes[i];
+    
+  
+      
+      noOfNotes[i].innerText = numberOfNotes;
     }
-
-}
+  }
